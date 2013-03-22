02f71ccc6dc8af13cd122b5c3b0e5bfc13405395
Piero Donaggio <piero.donaggio@trya.it>
2013-03-22 19:10:15 +0100

    First commit

---
+++ src/ImageReader.cpp
@@ -0,0 +1,119 @@
+/**
+ * @brief Acquisition library for images.
+ *
+ * @copyright Copyright 2013, Trya Srl
+ * via Siemens 19 - 39100 Bolzano BZ, ITALY
+ *
+ * @author Piero Donaggio <piero.donaggio@trya.it>
+ * @file ImageReader.cpp
+ */
+
+#include "ImageReader.h"
+
+#include <algorithm>
+#include <glog/logging.h>
+#include <boost/filesystem.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+ImageReader::ImageReader() :
+    mLastImageIndex(0)
+{
+    mFileNames.clear();
+}
+
+ImageReader::ImageReader(const std::string &imageDirectory) :
+    mLastImageIndex(0)
+{
+    mFileNames.clear();
+
+    (*this)(imageDirectory);
+}
+
+void
+ImageReader::operator() (const std::string &imageDirectory)
+{
+    namespace fs = boost::filesystem;
+    fs::path inputDir(imageDirectory);
+
+    if (fs::exists(inputDir) && fs::is_directory(inputDir))
+    {
+        mFileNames.clear();
+        fs::directory_iterator end, it(inputDir);
+
+        mFilesPath = it->path().parent_path().string() + "/";
+        LOG(INFO) << "Set image directory to: " << mFilesPath;
+
+        for (; it != end; ++it)
+        {
+            // File types must be supported by OpenCV
+            /// @todo There should be a bettwer way to check
+            /// file type/extension
+            if (it->path().extension() == ".tiff" ||
+                    it->path().extension() == ".JPG" ||
+                    it->path().extension() == ".jpg")
+            {
+                mFileNames.push_back(it->path().filename().string());
+            }
+        }
+
+        LOG(INFO) << "Number of images in the directory: " << mFileNames.size();
+
+        std::sort(mFileNames.begin(), mFileNames.end());
+    }
+    else
+    {
+        throw ImageReaderIOException("No such directory: " + imageDirectory);
+    }
+}
+
+const std::vector<std::string>&
+ImageReader::GetFileNames() const
+{
+    return mFileNames;
+}
+
+std::vector<cv::Mat>
+ImageReader::LoadAllImages() const
+{
+    std::vector<cv::Mat> imageSet;
+    typedef std::vector<std::string>::const_iterator fileIt;
+
+    LOG(INFO) << "Loading all image files...";
+    for (fileIt it = mFileNames.begin(); it != mFileNames.end(); ++it)
+    {
+        cv::Mat image = cv::imread(mFilesPath + *it, CV_LOAD_IMAGE_COLOR);
+        if (!image.data)
+        {
+            LOG(ERROR) << "Could not open image " << mFilesPath + *it;
+        }
+        else
+        {
+            imageSet.push_back(image);
+        }
+    }
+
+    return imageSet;
+}
+
+cv::Mat
+ImageReader::LoadNextImage()
+{
+    if (mLastImageIndex >= mFileNames.size())
+    {
+        // If the exception is caught, it is possible to reload
+        // the images from the beginning
+        mLastImageIndex = 0;
+        throw ImageReaderIOException("No more images to be loaded");
+    }
+
+    cv::Mat image = cv::imread(mFilesPath + mFileNames[mLastImageIndex],
+                               CV_LOAD_IMAGE_COLOR);
+
+    if (!image.data)
+        LOG(ERROR) << "Could not open image "
+        << mFilesPath + mFileNames[mLastImageIndex];
+
+    mLastImageIndex++;
+    return image;
+}
+
---
+++ src/ImageReader.h
@@ -0,0 +1,93 @@
+/**
+ * @brief Acquisition library for images.
+ *
+ * @copyright Copyright 2013, Trya Srl
+ * via Siemens 19 - 39100 Bolzano BZ, ITALY
+ *
+ * @author Piero Donaggio <piero.donaggio@trya.it>
+ * @file ImageReader.h
+ */
+
+#ifndef image_reader_h
+#define image_reader_h
+
+#include <stdexcept>
+#include <vector>
+#include <string>
+#include <opencv2/core/core.hpp>
+
+/**
+ * @class ImageReader
+ * @brief A helper class for loading images using OpenCV methods.
+ *
+ * This class does not attempt to load the images until a call to
+ * LoadAllImages() or LoadNextImage() is made. The rationale is to
+ * save some memory by not storing all the images, unless not stricty
+ * necessary.
+ */
+class ImageReader
+{
+public:
+
+    /**
+     * @brief Default constructor
+     */
+    ImageReader();
+    /**
+     * @brief Constructor from string
+     * @param[in] imageDirectory Name of the directory that contains the images
+     * to be loaded
+     */
+    ImageReader(const std::string &imageDirectory);
+    /**
+     * @brief Default destructor
+     */
+    virtual ~ImageReader();
+    /**
+     * @brief Set the source directory that contains the images
+     * @param[in] imageDirectory Name of the directory that contains the images
+     * to be loaded
+     */
+    void operator() (const std::string &imageDirectory);
+    /**
+     * @brief Retrieve image filenames in the source directory
+     * @return A vector of filenames
+     */
+    const std::vector<std::string>& GetFileNames() const;
+    /**
+     * @brief Load all images in the source directory
+     * @return A vector of images
+     */
+    std::vector<cv::Mat> LoadAllImages() const;
+    /**
+     * @brief Load the next image in the source directory
+     * @return An image
+     */
+    cv::Mat LoadNextImage();
+
+private:
+
+    /**
+     * @brief A list of image filenames
+     */
+    std::vector<std::string> mFileNames;
+    /**
+     * @brief The image file path
+     */
+    std::string mFilesPath;
+    /**
+     * @brief The index of the last loaded image
+     */
+    size_t mLastImageIndex;
+};
+
+inline ImageReader::~ImageReader() {};
+
+class ImageReaderIOException : public std::runtime_error
+{
+public:
+    ImageReaderIOException(const std::string &msg = "") :
+        runtime_error(msg) {}
+};
+
+#endif // header guard
