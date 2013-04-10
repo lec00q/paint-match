54d8a1be81aaa459e44155e02862309b73373e06
Piero Donaggio <piero.donaggio@trya.it>
2013-04-11 00:26:15 +0200

    fix -- nonfree needed to use SURF

--- src/ImageMatcher.cpp
+++ src/ImageMatcher.cpp
@@ -15,7 +15,7 @@
 #include <sstream>
 #include <algorithm>
 #include <numeric>
-#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
