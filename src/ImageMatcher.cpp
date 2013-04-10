a97292cc8f6268b26b0b95fcefb873ba6615c2c2
Piero Donaggio <piero.donaggio@trya.it>
2013-04-11 00:29:12 +0200

    Added both libraries

--- src/ImageMatcher.cpp
+++ src/ImageMatcher.cpp
@@ -16,6 +16,7 @@
 #include <algorithm>
 #include <numeric>
 #include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
